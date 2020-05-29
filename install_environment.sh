467e178bf96257d51e25411d3b0941de086d63ed
armavox <armavox@gmail.com>
2020-05-30 00:24:25 +0300

    install environment script

--- install_environment.sh
+++ install_environment.sh
@@ -11,10 +11,17 @@ else
         wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
         bash Miniconda3-latest-Linux-x86_64.sh
 
-    else [[ "$OSTYPE" == "darwin"* ]]
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
 
         wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
         bash Miniconda3-latest-MacOSX-x86_64.sh
+
+    else
+
+        echo Unrecignized system.
+        echo Please, install conda manually from \
+        https://conda.io/projects/conda/en/latest/user-guide/install/index.html. Then rerun this script
+        exit 
 
     fi
     
